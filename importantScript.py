67743b83830b9109910d2ee36575e34db7f3595d
ssarcoss <sarah.e.arcos@gmail.com>
2016-02-10 20:52:32 -0600

    I made it better

--- importantScript.py
+++ importantScript.py
@@ -1,1 +1,10 @@
-print("Sarah is a booty girl.")
+who = input('Who is a booty girl? : ')
+who = who.lower()
+
+if who == 'nick':
+	print("That's right, Nick is a booty girl.")
+elif who == 'sarah':
+	print("But it do.")
+else:
+	print("I don't know who that is, but Nick is the bootiest booty that ever bootied.")
+
